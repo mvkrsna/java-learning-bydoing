a60316eee9cd92891ad9d1f1e4582c182bee8e3b
mvkrsna <krsna.genai@gmail.com>
2025-11-08 01:12:07 -0600

    try and finally Questions

--- src/main/java/tryCatchQuestions/ReturnInTryFinally.java
+++ src/main/java/tryCatchQuestions/ReturnInTryFinally.java
@@ -1,4 +1,21 @@
 package tryCatchQuestions;
 
 public class ReturnInTryFinally {
+
+    // Example to demonstrate return statement in try and finally block
+    // Return statement in finally block overrides the return statement in try block
+    public static int testMethod() {
+        int x=10;
+        try {
+            System.out.println("In Try Block");
+            return x;
+        } finally {
+            System.out.println("In Finally Block");
+            x = 20;
+            return x;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(testMethod());
+    }
 }
--- src/main/java/tryCatchQuestions/TryCatchFinally.java
+++ src/main/java/tryCatchQuestions/TryCatchFinally.java
@@ -1,4 +1,15 @@
 package tryCatchQuestions;
 
 public class TryCatchFinally {
+
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("try Block Executed");
+        } finally {
+            System.out.println("finally Block Executed");
+        }
+
+
+    }
 }
