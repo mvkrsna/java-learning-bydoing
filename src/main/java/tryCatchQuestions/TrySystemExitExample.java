ac1a724bd414112e54ab809aed5c62bfc48e46bf
mvkrsna <krsna.genai@gmail.com>
2025-11-08 01:14:43 -0600

    try and finally Questions

--- src/main/java/tryCatchQuestions/TrySystemExitExample.java
+++ src/main/java/tryCatchQuestions/TrySystemExitExample.java
@@ -1,4 +1,15 @@
 package tryCatchQuestions;
 
 public class TrySystemExitExample {
+
+    // Example to demonstrate System.exit() in try block
+    // finally block will not be executed if System.exit() is called in try block
+    public static void main(String[] args) {
+        try {
+            System.out.println("In Try Block");
+            System.exit(0);
+        } finally {
+            System.out.println("In Finally Block");
+        }
+    }
 }
